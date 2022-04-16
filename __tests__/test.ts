cf04b9533ec267a081425be014721f40351b955c
Elliana May <me@mause.me>
2022-04-16 23:37:10 +0800

    feat: add moxios

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -1,5 +1,9 @@
 import comp from "../index";
+import moxios from 'moxios';
 
+beforeEach(() => moxios.install());
+afterEach(() => moxios.uninstall());
 it("boop", async () => {
+    moxios.stubRequest(/discussions/, {status: 200, response: {}});
   await comp.run({$: {}});
 });
--- index.ts
+++ index.ts
@@ -6,9 +6,13 @@ export default defineComponent({
   version: "0.0.1",
   props: {},
   async run({ $ }) {
-    await axios($, {
-      method: "POST",
-      url: "https://www.loomio.org/api/v1/discussions",
-    });
+    try {
+      await axios($, {
+        method: "POST",
+        url: "https://www.loomio.org/api/v1/discussions",
+      });
+    } catch (e) {
+      console.error(e);
+    }
   },
 });
