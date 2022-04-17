fce952b8e0f5be016b923ed745947d2136c33027
Elliana <me@mause.me>
2022-04-17 19:42:50 +0800

    chore: eslint should ignore package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     project: ["tsconfig.json"],
   },
   plugins: ["@typescript-eslint"],
-  ignorePatterns: [".eslintrc.js", "tsconfig.json"],
+  ignorePatterns: ["package.json", ".eslintrc.js", "tsconfig.json"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
