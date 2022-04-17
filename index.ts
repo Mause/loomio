22f597671b5ca8bbcb1e5566de5a04dee072b286
Elliana May <me@mause.me>
2022-04-17 14:13:12 +0800

    chore: send username too

--- index.ts
+++ index.ts
@@ -1,18 +1,27 @@
 import { axios } from "@pipedream/platform";
 import { defineComponent } from "ironpipe";
 import FormData from "form-data";
+import { AxiosError } from "axios";
+
+function isAxiosError(e: any): e is AxiosError {
+  return e.isAxiosError;
+}
 
 export default defineComponent({
   name: "loomio",
   version: "0.0.1",
   props: {
     username: { type: "string" },
+    email: { type: "string" },
+    password: { type: "string" },
   },
   methods: {
     async getCookie($: any) {
       const fd = new FormData();
-      fd.append("username", this.username as string);
+      fd.append("name", this.username as string);
+      fd.append("email", this.email as string);
       fd.append("password", this.password as string);
+      console.log(fd.getBuffer().toString());
       const res = await axios($, {
         method: "POST",
         url: "https://www.loomio.org/api/v1/sessions",
@@ -24,14 +33,15 @@ export default defineComponent({
     },
   },
   async run({ $ }) {
-    const cookies = await this.getCookie($);
     try {
+      const cookies = await this.getCookie($);
       await axios($, {
         method: "POST",
         url: "https://www.loomio.org/api/v1/discussions",
+        headers: { cookie: cookies },
       });
     } catch (e) {
-      console.error(e);
+      if (isAxiosError(e)) console.error(e.response?.data);
       throw e;
     }
   },
--- orch.ts
+++ orch.ts
@@ -1,9 +1,33 @@
 import comp from "./index";
 
-async function main() {
-  const self = Object.assign({}, comp.props, comp.methods);
+import dotenv from "dotenv";
 
-  await comp.run.call(self, { $: {} });
+const config = dotenv.config();
+
+function fromPairs<T>(iter: [string, T][]): Record<string, T> {
+  const res: Record<string, T> = {};
+  for (const [key, value] of iter) res[key] = value;
+  return res;
+}
+
+async function main() {
+  const props: Record<keyof typeof comp.props, any> = {
+    username: config.parsed!.USERNAME,
+    email: config.parsed!.EMAIL,
+    password: config.parsed!.PASSWORD,
+  };
+  const self = Object.create(
+    Object.assign(
+      {
+        run: comp.run,
+      },
+      props,
+      // fromPairs(Object.entries(comp.props)),
+      comp.methods
+    )
+  );
+
+  await self.run({ $: {} });
 }
 
 main().then(() => process.exit());
