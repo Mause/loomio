d283fb96092b76335cde28b690ab9d235461eb86
Elliana May <me@mause.me>
2022-04-18 11:42:41 +0800

    feat: add eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,15 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ["tsconfig.json"],
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "jest"],
   ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "jest/no-hooks": "off",
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/all",
   ],
 };
