157052116c8ec48dbe96b90ca52b9e41d1f17ea8
Elliana May <me@mause.me>
2022-04-21 17:05:10 +0800

    feat: add initial rewrite script

---
+++ src/rewrite.ts
@@ -0,0 +1,37 @@
+import { last } from "lodash";
+import { Project, SyntaxKind } from "ts-morph";
+
+function rewrite() {
+  const proj = new Project({ tsConfigFilePath: "dist/tsconfig.json" });
+
+  const dir = proj.getDirectories()[0]!;
+  const sf = dir.getSourceFileOrThrow("loomio.app.js");
+
+  const target1 = sf.getChildren()[0]!;
+  const target2 = last(target1.getChildren())!;
+
+  const binexp = target2.getChildrenOfKind(SyntaxKind.BinaryExpression)[0]!;
+
+  const ref = binexp
+    .getRight()!
+    .asKindOrThrow(SyntaxKind.Identifier)
+    .findReferences()[0]!;
+
+  const def = ref
+    .getDefinition()
+    .getDeclarationNode()!
+    .asKindOrThrow(SyntaxKind.VariableDeclaration);
+
+  const call = def.getInitializerIfKindOrThrow(SyntaxKind.CallExpression);
+
+  const args = call.getArguments();
+  if (args.length !== 1) throw new Error();
+
+  const theReplacement = args.at(0);
+
+  binexp.getRight()!.replaceWithText(theReplacement!.print());
+
+  console.log(sf.print());
+}
+
+rewrite();
