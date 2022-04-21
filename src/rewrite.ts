3bdb4c6fbcf3946cce19177223401d41d7af8d4c
Elliana May <me@mause.me>
2022-04-21 22:28:28 +0800

    chore: handle direct references too

--- src/rewrite.ts
+++ src/rewrite.ts
@@ -1,42 +1,62 @@
 import { last } from "lodash";
-import { Project, SourceFile, SyntaxKind } from "ts-morph";
+import {
+  CallExpression,
+  Project,
+  SourceFile,
+  SyntaxKind,
+  VariableDeclaration,
+} from "ts-morph";
 
 const ALLOW_LIST = new Set(["loomio.app", "create-poll", "create-discussion"]);
 
 function rewrite() {
+  console.time("loading project");
   const proj = new Project({ tsConfigFilePath: "dist/tsconfig.json" });
+  console.timeEnd("loading project");
 
   const dir = proj.getDirectories()[0]!;
 
-  for (const sf of dir.getSourceFiles()) {
+  for (const sf of dir.getDescendantSourceFiles()) {
+    console.time(sf.getBaseName());
     if (ALLOW_LIST.has(sf.getBaseNameWithoutExtension())) {
-      console.time(sf.getBaseName());
       fixFile(sf);
       sf.saveSync();
-      console.timeEnd(sf.getBaseName());
     }
+    console.timeEnd(sf.getBaseName());
   }
 }
 
 function fixFile(sf: SourceFile) {
-  const target1 = sf.getChildren()[0]!;
-  const target2 = last(target1.getChildren())!;
+  const binexp = sf
+    .getFirstDescendantOrThrow((node) => {
+      const isbin = node.getKind() === SyntaxKind.BinaryExpression;
 
-  const binexp = target2.getChildrenOfKind(SyntaxKind.BinaryExpression)[0]!;
+      return !!(
+        isbin &&
+        node.asKindOrThrow(SyntaxKind.BinaryExpression).getLeft().print() ==
+          "exports.default"
+      );
+    })
+    .asKindOrThrow(SyntaxKind.BinaryExpression);
 
-  const ref = binexp
-    .getRight()!
-    .asKindOrThrow(SyntaxKind.Identifier)
-    .findReferences()[0]!;
+  const right = binexp.getRight()!;
 
-  const def = ref
-    .getDefinition()
-    .getDeclarationNode()!
-    .asKindOrThrow(SyntaxKind.VariableDeclaration);
+  let call: CallExpression;
+  if (right.getKind() == SyntaxKind.Identifier) {
+    const ref = right.asKindOrThrow(SyntaxKind.Identifier).findReferences()[0]!;
 
-  const call = def.getInitializerIfKindOrThrow(SyntaxKind.CallExpression);
+    const def = ref
+      .getDefinition()
+      .getDeclarationNode()!
+      .asKindOrThrow(SyntaxKind.VariableDeclaration);
+    call = def.getInitializerIfKindOrThrow(SyntaxKind.CallExpression);
+  } else if (right.getKind() == SyntaxKind.CallExpression) {
+    call = right.asKindOrThrow(SyntaxKind.CallExpression);
+  } else {
+    throw new Error();
+  }
 
-  console.log(call.getExpression().print());
+  //console.log(call.getExpression().print());
 
   const args = call.getArguments();
   if (args.length !== 1) throw new Error();
@@ -45,7 +65,7 @@ function fixFile(sf: SourceFile) {
 
   binexp.getRight()!.replaceWithText(theReplacement!.print());
 
-  console.log(sf.print());
+  //console.log(sf.print());
 }
 
 rewrite();
